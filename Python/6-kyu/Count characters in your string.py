f90883597e26c90d72cd8c70c627a4ef40037e49
Timur <140198814+satymbaevtimur@users.noreply.github.com>
2023-10-13 17:54:14 +0600

    Create Count characters in your string.py

---
+++ Python/6-kyu/Count characters in your string.py
@@ -0,0 +1,8 @@
+def count(string):
+    char_count = {}
+    for char in string:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
