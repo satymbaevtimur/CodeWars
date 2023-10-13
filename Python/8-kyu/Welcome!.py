c2ee3d35b127ec835a865a79228e33390a5453d0
Timur <140198814+satymbaevtimur@users.noreply.github.com>
2023-10-13 16:13:16 +0600

    Create Welcome!.py

---
+++ Python/8-kyu/Welcome!.py
@@ -0,0 +1,29 @@
+def greet(language):
+    language_database = [
+        ("english", "Welcome"),
+        ("czech", "Vitejte"),
+        ("danish", "Velkomst"),
+        ("dutch", "Welkom"),
+        ("estonian", "Tere tulemast"),
+        ("finnish", "Tervetuloa"),
+        ("flemish", "Welgekomen"),
+        ("french", "Bienvenue"),
+        ("german", "Willkommen"),
+        ("irish", "Failte"),
+        ("italian", "Benvenuto"),
+        ("latvian", "Gaidits"),
+        ("lithuanian", "Laukiamas"),
+        ("polish", "Witamy"),
+        ("spanish", "Bienvenido"),
+        ("swedish", "Valkommen"),
+        ("welsh", "Croeso")
+    ]
+
+    if isinstance(language, str):
+        language = language.lower()
+
+    for lang, greeting in language_database:
+        if language == lang:
+            return greeting
+
+    return "Welcome"
