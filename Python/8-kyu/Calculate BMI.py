25c8ac927a6d9ef8f229b8d2d3f7251463f879e7
Timur <140198814+satymbaevtimur@users.noreply.github.com>
2023-11-10 02:16:13 +0600

    Create Calculate BMI.py

---
+++ Python/8-kyu/Calculate BMI.py
@@ -0,0 +1,11 @@
+def bmi(weight, height):
+    bmi_value = weight / (height ** 2)
+
+    if bmi_value <= 18.5:
+        return "Underweight"
+    elif bmi_value <= 25.0:
+        return "Normal"
+    elif bmi_value <= 30.0:
+        return "Overweight"
+    else:
+        return "Obese"
