684d1b892549bf8112468129d3eb589effda2e31
Timur <140198814+satymbaevtimur@users.noreply.github.com>
2023-11-13 13:57:59 +0600

    Create Get the Middle Character.py

---
+++ Python/7-kyu/Get the Middle Character.py
@@ -0,0 +1,8 @@
+def get_middle(s):
+    length = len(s)
+    middle_index = length // 2
+
+    if length % 2 == 0:
+        return s[middle_index - 1:middle_index + 1]
+    else:
+        return s[middle_index]
